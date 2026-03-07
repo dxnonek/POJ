03062682a100b4ecdc026e08533163f9490cea26
dxnonek <vipdominikpl135@gmail.com>
2026-03-07 12:47:15 +0100

    Zadanie 1

---
+++ src/Zadanie_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Zadanie_1 {
+    static void main(String[] args){
+        Scanner kok = new Scanner(System.in);
+        double a = kok.nextDouble();
+        double b = kok.nextDouble();
+        double wynik = a * b;
+        System.out.println(wynik);
+
+    }
+}
