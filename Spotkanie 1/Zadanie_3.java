6b54734977754aa4f0c478178d278a880b419a4e
dxnonek <vipdominikpl135@gmail.com>
2026-03-07 13:54:53 +0100

    Spotkanie 1

---
+++ Spotkanie 1/Zadanie_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Zadanie_3{
+    public static void main(String[] args){
+        Scanner napis = new Scanner(System.in);
+        String napis1 = napis.next();
+        String napis2 = napis.next();
+        System.out.println("\"%" + napis2 + " " + napis1 + "%\"");
+    }
+}
